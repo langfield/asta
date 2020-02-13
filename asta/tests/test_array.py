b21191512d7d11d5d3dbf65a20e53604ebf0ac6c
Brendan Whitaker <whitaker.213@osu.edu>
2020-02-12 22:59:05 -0700

    Passing second test.
    Need to fix mypy error caused by isinstance check.

--- asta/tests/test_array.py
+++ asta/tests/test_array.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """ Tests for the 'Array' typing class. """
-import hypothesis.strategies as st
 import hypothesis.extra.numpy as hnp
 from hypothesis import given
 
 from asta import Array
-from asta.tests import strategies as strats
 
 # pylint: disable=no-value-for-parameter
 
 
-@given(hnp.arrays(dtype=strats.dtypes(), shape=strats.shapes()))
+@given(hnp.arrays(dtype=hnp.scalar_dtypes(), shape=hnp.array_shapes(min_dims=0)))
 def test_array_passes_generic_isinstance(arr: Array) -> None:
     """ Make sure a generic numpy array is an instance of 'Array'. """
-    pass
+    assert isinstance(arr, Array)
+
+
+@given(hnp.arrays(dtype=hnp.scalar_dtypes(), shape=hnp.array_shapes(min_dims=0)))
+def test_array_isinstance_npdtype_shape(arr: Array) -> None:
+    """ Tests that an array is an instance of 'Array[(<dtype>,)+shape]'. """
+    arg: tuple = (arr.dtype,) + arr.shape
+    assert isinstance(arr, Array[arg])
