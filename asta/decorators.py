3cc38bdef0b0468992a8b5ad3854e3b2e482d7a0
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2020-02-20 17:46:57 -0700

    Added module for toggling typechecking.
    Cleaned up typecheck printing in decorator.

--- asta/decorators.py
+++ asta/decorators.py
@@ -34,21 +34,24 @@ def typechecked(decorated):  # type: ignore[no-untyped-def]
         """ Decorated/typechecked function. """
 
         for i, arg in enumerate(args):
-            param = sig.parameters[param_names[i]]
+            name = param_names[i]
+            param = sig.parameters[name]
             if isinstance(param.annotation, (_ArrayMeta, _TensorMeta)):
                 if not isinstance(arg, param.annotation):
-                    type_err = f"Argument value '{arg}' for parameter '{param.name}' "
+                    type_err = f"Argument value for parameter '{param.name}' "
                     type_err += f"has wrong type. Expected type: '{param.annotation}' "
                     type_err += f"Actual type: '{type(arg)}'"
                     raise TypeError(type_err)
+                print(f"PASSED: Arg '{name}' matched parameter '{param.annotation}'.")
 
         ret = decorated(*args, **kwargs)
         if isinstance(sig.return_annotation, (_ArrayMeta, _TensorMeta)):
             if not isinstance(ret, sig.return_annotation):
-                type_err = f"Return value '{ret}' has wrong type. "
+                type_err = f"Return value has wrong type. "
                 type_err += f"Expected type: '{sig.return_annotation}' "
                 type_err += f"Actual type: '{type(ret)}'"
                 raise TypeError(type_err)
+            print(f"PASSED: Return matched return type '{sig.return_annotation}'.")
 
         return ret
 
