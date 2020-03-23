d10772cc7b3329ab20b4e82e03c21507f3445831
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2020-03-23 14:05:27 -0600

    Added more functions for config parsing.
    Added to README.
    Added '_internal' storage module.

---
+++ asta/_internal.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" A module for storing python objects for internal asta use. """
+from typing import Dict, Any
+
+# pylint: disable=redefined-outer-name
+
+storage: Dict[str, Any]
+
+
+def __getattr__(name: str) -> Any:
+    """ Yields the objects. """
+    try:
+        try:
+            return storage[name]
+        except KeyError:
+            return None
+    except NameError:
+        return None
+
+
+def __setattr__(name: str, value: Any) -> None:
+    """ Sets the objects. """
+    try:
+        storage
+    except NameError:
+        storage = {}
+    storage[name] = value
